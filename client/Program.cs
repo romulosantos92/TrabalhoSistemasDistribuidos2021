bada5cd59109cb00f6474bdae21a3cb266c5fcd3
Rômulo Santos <romulosantos1092@gmail.com>
2021-07-04 23:55:24 -0300

    Versao principal funcionando

--- client/Program.cs
+++ client/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Net.Http;
+using Grpc.Net.Client;
+using System.Threading;
+using System.Threading.Tasks;
+using server;
 
 namespace client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Consuming a GRPC service");
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = 
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient});
+            var client = new Users.UsersClient(channel);
+            try
+            {
+                UserRequest request = new UserRequest() { CompanyId = 1 };
+                using (var call = client.GetUsers(request))
+                {
+                    //stream
+                    while( await call.ResponseStream.MoveNext(CancellationToken.None))
+                    {
+                        var currentUser = call.ResponseStream.Current;
+                        Console.WriteLine(currentUser.FirstName + " " + currentUser.LastName + " is being fetched by the service");
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- server/Startup.cs
+++ server/Startup.cs
@@ -32,6 +32,7 @@ namespace server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<UserService>();
 
                 endpoints.MapGet("/", async context =>
                 {
